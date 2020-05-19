455492abf3197135f28fce21cef07040d2c5aa3c
MaryamTalal <maryamtselim@gmail.com>
2020-05-19 17:19:05 +0200

    Updates

---
+++ precache-manifest.0301f219981c5be603435143f4bdcfdc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301c9409c184ffd2f03fc45f1bcb7e9f",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "7cfb255016bc63cf8020",
+    "url": "/react-gh-pages/static/css/main.c690d853.chunk.css"
+  },
+  {
+    "revision": "df58767377b96f3cf416",
+    "url": "/react-gh-pages/static/js/2.f9edcc46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.f9edcc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfb255016bc63cf8020",
+    "url": "/react-gh-pages/static/js/main.436e66ed.chunk.js"
+  },
+  {
+    "revision": "926ddeb10cd4e3210b72",
+    "url": "/react-gh-pages/static/js/runtime-main.0cbf1afa.js"
+  },
+  {
+    "revision": "fcfeff3744512ef19a21f33ab9223617",
+    "url": "/react-gh-pages/static/media/LeckerliOne-Regular.fcfeff37.otf"
+  },
+  {
+    "revision": "397a56821c43e85da7725a53cd51ec9b",
+    "url": "/react-gh-pages/static/media/Muli Light.397a5682.ttf"
+  },
+  {
+    "revision": "4a95f2ac02d75ed6f2b9f4afaf90ac7a",
+    "url": "/react-gh-pages/static/media/No. Seven Regular.4a95f2ac.otf"
+  }
+]);
